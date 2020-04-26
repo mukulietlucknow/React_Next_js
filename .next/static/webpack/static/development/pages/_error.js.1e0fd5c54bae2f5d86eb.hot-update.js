40b4c10bf539dee38b6c98e30ae9449fbac885dd
mukulietlucknow <mukul.ietlucknow@gmail.com>
2020-04-26 12:21:24 +0530

    intro is done

---
+++ .next/static/webpack/static/development/pages/_error.js.1e0fd5c54bae2f5d86eb.hot-update.js
@@ -0,0 +1,136 @@
+webpackHotUpdate("static\\development\\pages\\_error.js",{
+
+/***/ "./node_modules/@babel/runtime/helpers/arrayWithoutHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/iterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/nonIterableSpread.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/toConsumableArray.js":
+false,
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=D%3A%5CReact%5CReact_Next_js%5Cpages%5C_error.js!./":
+/*!*******************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=D%3A%5CReact%5CReact_Next_js%5Cpages%5C_error.js ***!
+  \*******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/_error", function() {
+      var mod = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/_error.js */ "./pages/_error.js", function() {
+          if(!next.router.components["/_error"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+          next.router.update("/_error", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=next%2Fdist%2Fpages%2F_error!./":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/amp.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head-manager-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/head.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/side-effect.js":
+false,
+
+/***/ "./node_modules/next/dist/pages/_error.js":
+false,
+
+/***/ "./pages/_error.js":
+/*!*************************!*\
+  !*** ./pages/_error.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _this = undefined,
+    _jsxFileName = "D:\\React\\React_Next_js\\pages\\_error.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var errorPage = function errorPage() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 9
+    }
+  }, "Oops, something went wrong."), __jsx("p", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 9
+    }
+  }, "Try ", __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 16
+    }
+  }, __jsx("a", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 31
+    }
+  }, "going back")), "."));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (errorPage);
+
+/***/ }),
+
+/***/ 2:
+/*!***********************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F_error&absolutePagePath=D%3A%5CReact%5CReact_Next_js%5Cpages%5C_error.js ***!
+  \***********************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F_error&absolutePagePath=D%3A%5CReact%5CReact_Next_js%5Cpages%5C_error.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=D%3A%5CReact%5CReact_Next_js%5Cpages%5C_error.js!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.1e0fd5c54bae2f5d86eb.hot-update.js.map
